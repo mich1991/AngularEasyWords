5275fbe084a8f89c6b195a2becc2371c3569ae4a
mich1991 <wozniak.michal91@gmail.com>
2021-03-20 14:16:45 +0000

    Service added , project finished

--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { WordType } from '../data/models';
+import { WordsService } from '../services/words.service';
 
 @Component({
   selector: 'app-questions',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionsComponent implements OnInit {
 
-  constructor() { }
+  word?: WordType = undefined ;
+
+  constructor(private wordsService : WordsService) { }
 
   ngOnInit(): void {
+    this.fetchWord()
   }
 
+  addToNouns(word: WordType){
+    this.wordsService.addNoun(word)
+    this.fetchWord()
+  }
+
+  addToVerbs(word: WordType){
+    this.wordsService.addVerb(word)
+    this.fetchWord()
+  }
+  check(){
+    this.wordsService.check()
+  }
+
+  private fetchWord():void {
+    this.word = this.wordsService.getWords().shift()
+  }
+  
 }
